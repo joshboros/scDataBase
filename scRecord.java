f000a81a05748d0828bea906d4380233f01c4735
Josh Boros <josh.boros@gmail.com>
2017-11-17 07:00:00 -0600

    rename entities to mach scRecord

--- scRecord.java
+++ scRecord.java
@@ -5,46 +5,50 @@
  */
 public class scRecord {
     private String subject;
-    private int section;
-    private int subsection;
+    private Integer section;
+    private Integer subsection;
     private String topic;
-    private String instructionsPlainText;
+    private DifficultyLevel difficulty;
+    private String instructions;
 
-    private String questionPlainText;
+    //private String questionPlainText;
     private String questionHtml;
     private String questionLatex;
-
+    private String choicesHtml;
     private String answerPlainText;
-    private String[] htmlChoices;
-    private String answerLatex;
     private String answerHtml;
+
+    private String questionJeopardy;
+    private String answerJeopardy;
 
     public String toString(){
         StringBuffer sb = new StringBuffer();
         sb.append("Subject: " + getSubject());
         sb.append(System.lineSeparator());
-        sb.append("Section: " + getSection());
+        sb.append("Section: " + getSection().toString());
         sb.append(System.lineSeparator());
-        sb.append("Subsection: " + getSubsection());
+        sb.append("Subsection: " + getSubsection().toString());
         sb.append(System.lineSeparator());
         sb.append("Topic: " + getTopic());
         sb.append(System.lineSeparator());
+        sb.append("Difficulty: " + getDifficulty().toString());
+        sb.append(System.lineSeparator());
+        sb.append("Instructions: " + getInstructions());
+        sb.append(System.lineSeparator());
         sb.append("Question: ");
         sb.append(System.lineSeparator());
-        sb.append("\tPlain Text: " + getQuestionPlainText());
-        sb.append(System.lineSeparator());
-        sb.append("\tHTML: " + getQuestionPlainText());
+        sb.append("\tHTML: " + getQuestionHtml());
         sb.append(System.lineSeparator());
         sb.append("\tLATEX: " + getQuestionLatex());
         sb.append(System.lineSeparator());
-        sb.append("Answer: ");
+        sb.append("\tJeopardy: " + getQuestionJeopardy());
         sb.append(System.lineSeparator());
-        sb.append("\tPlain Text: " + getAnswerPlainText());
+        sb.append("Answer: ");
         sb.append(System.lineSeparator());
         sb.append("\tHTML: " + getAnswerHtml());
         sb.append(System.lineSeparator());
-        sb.append("\tLATEX: " + getAnswerLatex());
-        sb.append(System.lineSeparator());
+        sb.append("\tJeopardy: " + getAnswerJeopardy());
+        sb.append("Choices (HTML): " + getChoicesHtml());
 
         return sb.toString();
     }
@@ -66,8 +70,6 @@ public class scRecord {
          * Hard difficulty level.
          */
         HARD}
-    private DifficultyLevel difficulty;
-
 
     /**
      * Gets difficulty.
@@ -110,7 +112,7 @@ public class scRecord {
      *
      * @return the section
      */
-    public int getSection() {
+    public Integer getSection() {
         return section;
     }
 
@@ -128,7 +130,7 @@ public class scRecord {
      *
      * @return the subsection
      */
-    public int getSubsection() {
+    public Integer getSubsection() {
         return subsection;
     }
 
@@ -160,40 +162,21 @@ public class scRecord {
     }
 
     /**
-     * Get the instructions in plain text.
+     * Get the instructions.
      *
      * @return the string
      */
-    public String getInstructionsPlainText(){
-        return instructionsPlainText;
+    public String getInstructions(){
+        return instructions;
     }
 
     /**
-     * Set instructions plain text.
+     * Set instructions.
      *
-     * @param instructionsPlainText the instructions plain text
+     * @param instructions the instructions plain text
      */
-    public void setInstructionsPlainText(String instructionsPlainText){
-        this.instructionsPlainText = instructionsPlainText;
-    }
-
-
-    /**
-     * Gets the question in plain text.
-     *
-     * @return the question plain text
-     */
-    public String getQuestionPlainText() {
-        return questionPlainText;
-    }
-
-    /**
-     * Sets question plain text.
-     *
-     * @param questionPlainText the question plain text
-     */
-    public void setQuestionPlainText(String questionPlainText) {
-        this.questionPlainText = questionPlainText;
+    public void setInstructions(String instructions){
+        this.instructions = instructions;
     }
 
     /**
@@ -203,9 +186,6 @@ public class scRecord {
      * @return the question html
      */
     public String getQuestionHtml() {
-        if (questionHtml.isEmpty()){
-            return questionPlainText;
-        }
         return questionHtml;
     }
 
@@ -225,9 +205,6 @@ public class scRecord {
      * @return the question latex
      */
     public String getQuestionLatex() {
-        if (questionLatex.isEmpty()){
-            return questionPlainText;
-        }
         return questionLatex;
     }
 
@@ -286,39 +263,47 @@ public class scRecord {
      *
      * @return the string [ ]
      */
-    public String[] getHtmlChoices() {
-        return htmlChoices;
+    public String getChoicesHtml() {
+        return choicesHtml;
     }
 
     /**
      * Sets html choices.
      *
-     * @param htmlChoices the html choices
+     * @param choicesHtml the html choices
      */
-    public void setHtmlChoices(String[] htmlChoices) {
-        this.htmlChoices = htmlChoices;
+    public void setChoicesHtml(String choicesHtml) {
+        this.choicesHtml = choicesHtml;
     }
 
-    /**
-     * Get the answer with latex markup.
-     * If no latex version found then return plain text.
-     *
-     * @return the answer latex
-     */
-    public String getAnswerLatex() {
-        if(answerLatex.isEmpty()){
-            return answerPlainText;
+    public String getQuestionJeopardy() {
+        return questionJeopardy;
+    }
+
+    public void setQuestionJeopardy(String questionJeopardy) {
+        this.questionJeopardy = questionJeopardy;
+    }
+
+    public String getAnswerJeopardy() {
+        return answerJeopardy;
+    }
+
+    public void setAnswerJeopardy(String answerJeopardy) {
+        this.answerJeopardy = answerJeopardy;
+    }
+/**
+    static class difficultyLevel{
+        private final Integer UNSPECIFIED = 0;
+        private final Integer EASY = 1;
+        private final Integer MEDIUM = 2;
+        private final Integer HARD =3;
+
+        private Integer level;
+
+        public  getLevel(){
+            return level;
         }
-        return answerLatex;
     }
 
-    /**
-     * Set answer with latex markup.
-     *
-     * @param answerLatex the answer latex
-     */
-    public void setAnswerLatex(String answerLatex) {
-        this.answerLatex = answerLatex;
-    }
-
+*/
 }
