31165eddca55a44a748b09a16693487654cb72b5
Josh Boros <josh.boros@gmail.com>
2017-11-18 18:16:15 -0600

    Class too manage the DOM

---
+++ domTool.java
@@ -0,0 +1,45 @@
+package school.termProject.database;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+
+
+class domTool {
+    private Document dbDom;
+    private final String domRoot = "TestBank";
+
+    private void createDom(File dbFileObj) {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db;
+
+        try {
+            //Using factory get an instance of document builder
+            db = dbf.newDocumentBuilder();
+
+            //parse using builder to get DOM representation of the XML file
+            dbDom = db.parse(dbFileObj);
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (SAXException se) {
+            se.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private void getNodeList(){
+        NodeList questionNodes = dbDom.getChildNodes();
+    }
+
+
+
+
+}
